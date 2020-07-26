f0a5dff2efe98e31123eaa92a24cdc657c46c8a7
saraajay11 <52280974+saraajay11@users.noreply.github.com>
2020-07-26 11:02:47 +0530

    Add files via upload

---
+++ DesignPatterns-master/DesignPatterns-master/DesignPatterns/src/main/java/com/epam/DesignPatterns/CreationalBuilderCoffee.java
@@ -0,0 +1,21 @@
+package com.epam.DesignPatterns;
+
+public class CreationalBuilderCoffee{
+    private String type;
+    private boolean sugar;
+    private boolean milk;
+    private int serves;
+    CreationalBuilderCoffee(String type,boolean sugar,boolean milk,int serves)
+    {
+    	this.type=type;
+    	this.sugar=sugar;
+    	this.milk=milk;
+    	this.serves=serves;
+    }
+
+public String toString() {
+return String.format
+("Coffee [Type=%s, Sugar=%s, Milk=%s, Serves=%d]", this.type, this.sugar, this.milk, this.serves);
+}
+}
+
